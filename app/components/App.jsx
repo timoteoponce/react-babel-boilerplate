376e5a9e9801b7598f22cbf77eed708da4c52700
Timoteo Ponce <timoteo.ponce@swissbytes.ch>
2016-07-11 16:49:40 -0400

    Added Ajax example

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
+import 'whatwg-fetch';
 
 class App extends React.Component{
 
   constructor(){
     super();
     this.state = {
-      name: "Timoteo Ponce"
+      name: "Timoteo Ponce",
+      remotePosts: null 
     };
   }
 
@@ -22,7 +24,19 @@ class App extends React.Component{
       <div>
         <h2>{this.state.name}</h2>  
         <input type="button" value="Say hello!" onClick={this.onSayHello.bind(this)}/>
+        <hr/>
+        {this.renderAjaxCall()}
       </div>    
+    );
+  }
+
+  renderAjaxCall(){
+    return(
+      <div>
+        <input type="button" value="Get Posts" onClick={this.onGetPosts.bind(this)}/>
+        <br/>
+        <textarea rows="10" cols="30" value={this.state.remotePosts} />
+      </div>
     );
   }
 
@@ -31,6 +45,17 @@ class App extends React.Component{
     this.setState({
       name: "Timo says hello!! at " + new Date().getTime()
     });
+  }
+
+  onGetPosts(){
+    let self = this;
+    fetch('http://jsonplaceholder.typicode.com/posts/1')
+      .then( (response) => response.json() )
+      .then( (json) => {
+        self.setState({
+          remotePosts: json
+        });
+      });
   }
 }
 
